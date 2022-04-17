65d7ab844569f99be6d5f566ae18585185f159f3
Yusuf Avcilar <yusufavcilar53@gmail.com>
2022-04-17 18:24:13 +0300

    javascript calculator project completed

---
+++ app.js
@@ -0,0 +1,32 @@
+let btn = document.querySelectorAll(".btn")
+let input = document.querySelector(".calculator__input")
+let equation = document.getElementById("equation")
+let clear = document.getElementById("delete")
+
+for (let i = 0; i <17; i++) {
+    if (i != 15 && i != 16){
+        btn[i].addEventListener("click",writer);
+    }   
+}
+function writer () {
+input.value = input.value + this.value;
+}
+
+equation.addEventListener("click", hesap)
+
+function hesap() {
+    input.value = eval(input.value)
+}
+
+clear.addEventListener("click", temiz);
+ 
+function temiz() {
+    input.value = "";
+}
+
+
+
+
+
+
+
